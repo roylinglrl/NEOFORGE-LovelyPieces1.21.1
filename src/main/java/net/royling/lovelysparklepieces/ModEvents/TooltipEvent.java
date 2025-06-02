6dabd34b66d4dbcca0e832fedbe85e2fdbe82633
roylinglrl <147955358@qq.com>
2025-06-02 20:59:05 +0800

    更新tooltipEvent 分离事件防止出现错误

--- src/main/java/net/royling/lovelysparklepieces/ModEvents/TooltipEvent.java
+++ src/main/java/net/royling/lovelysparklepieces/ModEvents/TooltipEvent.java
@@ -55,6 +55,7 @@ public class TooltipEvent {
                                         Component.translatable(getStageTranslationKey(stage)))
                                 .withStyle(ChatFormatting.BLUE))
                 );
+
                 break;
             }
         }
